fb90526063f2778808176d4834f6db01a7af403f
Miroslav <myroslav.bashkirov@gmail.com>
2023-09-13 17:25:06 +0300

    Add project files.

---
+++ HomeTasks_Pro_11/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace HomeTasks_Pro_11
+{
+    /*
+        Завдання 2
+
+        Створіть консольну програму, 
+        яка в різних потоках зможе отримати доступ до 2-х файлів. 
+        Вважайте з цих файлів вміст і спробуйте записати отриману інформацію в третій файл. 
+        Читання/запис мають здійснюватися одночасно у кожному з дочірніх потоків. 
+        Використовуйте блокування потоків для того, щоб досягти коректного запису в кінцевий файл.
+     */
+    internal class Program
+    {
+        static object locker = new object();
+
+        static void Main(string[] args)
+        {
+            
+            Thread thread = new Thread(() => ReadFileAndWriteToAnother("TextFile1.txt"));
+            Thread thread2 = new Thread(() => ReadFileAndWriteToAnother("TextFile2.txt"));
+            thread.Start();
+            thread2.Start();
+        }
+
+        static void ReadFileAndWriteToAnother(string path)
+        {
+            
+            string res;
+            using (StreamReader sr = new StreamReader(path)) 
+            {
+                res = sr.ReadToEnd();
+            }
+
+            lock(locker)
+            {
+                using (StreamWriter sr = new StreamWriter("text.txt", true))
+                {
+                    sr.WriteLine(res);
+                }
+            }
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static int counter = 0;
+
+        static object block = new object();
+
+        static void Function()
+        {
+            for (int i = 0; i < 50; ++i)
+            {
+              
+                Monitor.Enter(block); 
+
+             
+                Console.WriteLine(++counter);
+
+             
+                Monitor.Exit(block);
+            }
+        }
+
+        static void Main()
+        {
+            Thread[] threads = { new Thread(Function), new Thread(Function), new Thread(Function) };
+
+            foreach (Thread thread in threads)
+                thread.Start();
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
